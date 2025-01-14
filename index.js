8b41a4970b7c57167c7ecbe1502b355924a1aad2
mohamedsuhail065 <mohamedsuhail065@gmail.com>
2025-01-14 12:37:57 +0400

    db moved

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const session = require("express-session");
 const bodyParser = require("body-parser");
-const { poolPromise, sql, getSettings } = require("./views/db");
+const { poolPromise, sql, getSettings } = require("./db");
 const QRCode = require("qrcode");
 const PDFDocument = require("pdfkit");
 const app = express();
